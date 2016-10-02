0679d39f0900afbadf1b50df3d19d66e5a7460e2
Bejan Fozdar <ixyzt@users.noreply.github.com>
2016-10-02 14:41:02 -0400

    Complete

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -11,15 +11,96 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    
+    var statesAndCapitals = [
+        "Alabama":"Montgomery",
+        "Alaska":"Juneau",
+        "Arizona":"Phoenix",
+        "Arkansas":"Little Rock",
+        "California":"Sacramento",
+        "Colorado":"Denver",
+        "Connecticut":"Hartford",
+        "Delaware":"Dover",
+        "Florida":"Tallahassee",
+        "Georgia":"Atlanta",
+        "Hawaii":"Honolulu",
+        "Idaho":"Boise",
+        "Illinois":"Springfield",
+        "Indiana":"Indianapolis",
+        "Iowa":"Des Moines",
+        "Kansas":"Topeka",
+        "Kentucky":"Frankfort",
+        "Lousiana":"Baton Rouge",
+        "Maine":"Augusta",
+        "Maryland":"Annapolis",
+        "Massassachusetts":"Boston",
+        "Michigan":"Lansing",
+        "Minnesota":"Saint Paul",
+        "Mississippi":"Jackson",
+        "Missouri":"Jefferson City",
+        "Montana":"Helena",
+        "Nebraska":"Lincoln",
+        "Nevada":"Carson City",
+        "New Hampshire":"Concord",
+        "New Jersey":"Trenton",
+        "New Mexico":"Santa Fe",
+        "New York":"Albany",
+        "North Carolina":"Raleigh",
+        "North Dakota":"Bismarck",
+        "Ohio":"Columbus",
+        "Oklahoma":"Oklahoma City",
+        "Oregon":"Salem",
+        "Pennsylvania":"Harrisburg",
+        "Rhode Island":"Providence",
+        "South Carolina":"Columbia",
+        "South Dakota":"Pierre",
+        "Tennessee":"Nashville",
+        "Texas":"Austin",
+        "Utah":"Salt Lake City",
+        "Vermont":"Montpelier",
+        "Virgina":"Richmond",
+        "Washington":"Olympia",
+        "West Virginia":"Charleston",
+        "Wisconsin":"Madison",
+        "Wyoming":"Cheyenne"
+    ]
     
     
     func solveTrivia(_ trivia: [String : String]) -> String {
         
-        // Implement this function!
+        var notSimilar: String = ""
+        var stateName: String
+        var capitalName: String
+        var stateNameCharacters = [Character]()
+        var capitalNameCharacters = [Character]()
         
-        return "Hi :D"
         
+        for (state,capital) in statesAndCapitals {
+            var sharedArray = [String]()
+            stateName = state.lowercased()
+            capitalName = capital.lowercased()
+            for character in stateName.characters {
+                stateNameCharacters.append(character)
+                
+            }
+            for character in capitalName.characters {
+                capitalNameCharacters.append(character)
+            }
+            for each in stateNameCharacters {
+                if capitalNameCharacters.contains(each) {
+                    sharedArray.append(String(each))
+                    print("Shares \(each)")
+                }
+            }
+            if sharedArray.isEmpty {
+                notSimilar = "\(state)"
+            }
+            print("State Name: \(stateNameCharacters)")
+            print("Capital Name: \(capitalNameCharacters)")
+            stateNameCharacters.removeAll()
+            capitalNameCharacters.removeAll()
+        }
+        print(notSimilar)
+        return notSimilar
     }
     
     
